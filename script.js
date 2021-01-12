a54ba51351454f8352f03169adea9e916fff9384
ko951116 <62150431+ko951116@users.noreply.github.com>
2021-01-12 16:21:17 +0900

    initial push

---
+++ script.js
@@ -0,0 +1,51 @@
+//-------preloader--------//
+window.addEventListener("load", () => {
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(() => {
+        document.querySelector(".preloader").style.display="none";
+    },600)
+})
+
+    
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 200) {
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+ 
+
+    //-------slide-up--------//
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    //-------toggle menu/navbar--------//
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    //-------typing animation--------//
+    /* var typed = new Typed(".typing", {
+        strings: ["Interactive Design", "Human Computer Interaction", "Creative Techs", "User Experience"],
+        typeSpeed: 60,
+        backSpeed: 50,
+        loop: true
+    }); */
+    var typed = new Typed(".typing-2", {
+        strings: ["Interactive Design", "Human Computer Interaction", "Creative Techs", "User Experience"],
+        typeSpeed: 60,
+        backSpeed: 50,
+        loop: true
+    });
+
+});
